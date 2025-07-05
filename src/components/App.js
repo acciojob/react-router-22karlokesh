011d095d9957f0eb54520934610f4308c156b58a
Lokesh Baviskar <101720181+22karlokesh@users.noreply.github.com>
2025-07-06 00:41:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,47 @@
 
+// import React from "react";
+// import './../styles/App.css';
+
+// const App = () => {
+//   return (
+//     <div>
+//         {/* Do not remove the main div */}
+//     </div>
+//   )
+// }
+
+// export default App
+
+
 import React from "react";
-import './../styles/App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./../styles/App.css";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <BrowserRouter>
+        <nav>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/about">About</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+const Home = () => {
+  return <h1>Welcome to my website!</h1>;
+};
+
+const About = () => {
+  return <h1>This is a sample React Router program.</h1>;
+};
+
+export default App;
+
